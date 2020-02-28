8662aea382fd3aa15db39f7bc8c4c7967a74266e
Geoff Arroyo <garroyo@stedwards.edu>
2020-02-28 01:27:00 -0600

    Final round of test cases added.

--- test_login.py
+++ test_login.py
@@ -3,6 +3,7 @@ import requests
 import unittest
 
 class Test_Login_Py(unittest.TestCase):
+
     #Test 200 status response on GET request for login.py
     def test_login_get_status(self):
         url = "https://demand.team22.softwareengineeringii.com/loginHandler"
@@ -16,6 +17,7 @@ class Test_Login_Py(unittest.TestCase):
         statusCode = response.status_code
         self.assertEqual(statusCode, 200)
 
+    # Test 401 status response on POST request for login.py when the username isn't stored in our database
     def test_login_post_status_incorrect_username(self):
         url = "https://demand.team22.softwareengineeringii.com/loginHandler"
 
@@ -27,9 +29,61 @@ class Test_Login_Py(unittest.TestCase):
         response = requests.request("POST", url, headers=headers, data = payload)
         statusCode = response.status_code
         self.assertEqual(statusCode, 401)
+    
+    # Test 401 status on POST request for login.py when the password isn't stored in our database
+    def test_login_post_status_incorrect_password(self):
+        url = "https://demand.team22.softwareengineeringii.com/loginHandler"
+
+        payload = "{\"username\" : \"Geoff\", \"password\" : \"LOLPASSWORD\"}"
+        headers = {
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data = payload)
+        statusCode = response.status_code
+        self.assertEqual(statusCode, 401)
+
+    # Test 401 status on POST request for login.py when the password and username aren't stored in our database
+    def test_login_post_status_incorrect_password_incorrect_username(self):
+        url = "https://demand.team22.softwareengineeringii.com/loginHandler"
+
+        payload = "{\"username\" : \"randomuser\", \"password\" : \"randompassword\"}"
+        headers = {
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data = payload)
+        statusCode = response.status_code
+        self.assertEqual(statusCode, 401)
+
+    def test_successful_login_1(self):
+        url = "https://demand.team22.softwareengineeringii.com/loginHandler"
+
+        payload = "{\"username\" : \"Geoff\", \"password\" : \"HAHA\"}"
+        headers = {
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data = payload)
+        statusCode = response.status_code
+        self.assertEqual(statusCode, 200)
+
+    
+    def test_successful_login_1(self):
+        url = "https://demand.team22.softwareengineeringii.com/loginHandler"
+
+        payload = "{\"username\" : \"Michaels\", \"password\" : \"password\"}"
+        headers = {
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data = payload)
+        statusCode = response.status_code
+        self.assertEqual(statusCode, 200)
+    
+
 
 if __name__ == '__main__':
-    print("Main")
     unittest.main()
 
 
