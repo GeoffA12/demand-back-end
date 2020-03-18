e0a8e66b4776ef39e29832c49dba9db704ff6adc
garroyo <garroyo@stedwards.edu>
2020-03-17 19:03:29 -0500

    Adding python class definitions for an order and customer class. Adding unit test skeletons for both classes.

---
+++ customer.py
@@ -0,0 +1,24 @@
+
+# Python demand customer class. 
+# Note that if we decide to use inheritance in this class defintion, (e.g., customer extends User),
+# then we'll need to change our class defintion below
+# class Customer():
+
+    # Class constructor. Set instance variables below. 
+    # def __init__(self):
+        
+    #Defining our getter methods
+    # def getUsername(self):
+        
+
+    # def getPassword(self):
+
+    
+    # def getEmail(self):
+
+    
+    # def getPhone(self):
+
+    
+    # toString() method
+    # def __str__(self):
---
+++ order.py
@@ -0,0 +1,23 @@
+
+# Python demand order class. 
+# class Order():
+
+    # Class constructor. Set instance variables below. 
+    # def __init__(self):
+        
+    #Defining our getter methods
+    # This method might be a bit tricky due to the fact that we'll need to retrieve a piece of data 
+    # identifying the user who made the order from the front end (using session storage, might need to change up a little of 
+    # back end for this 
+    # but not sure yet).
+    # def getCustomerIdOfOrder(self):
+
+    
+    # def getOrderType(self):
+
+    
+    # def getDestinationAddress(self):
+
+    
+    # toString() method
+    # def __str__(self):
