8d571813f27ad5961fe4daf927eb7d83045636da
Daphne Domansi <ddomansi@stedwards.edu>
2020-03-27 06:08:32 +0000

    order.py edited online with Bitbucket (still in progress)

--- order.py
+++ order.py
@@ -1,22 +1,26 @@
 
 # Python demand order class. 
-# class Order():
+class Order():
 
     # Class constructor. Set instance variables below. 
     # def __init__(self):
+        
         
     #Defining our getter methods
     # This method might be a bit tricky due to the fact that we'll need to retrieve a piece of data 
     # identifying the user who made the order from the front end (using session storage, might need to change up a little of 
     # back end for this 
     # but not sure yet).
-    # def getCustomerIdOfOrder(self):
+    def getCustomerIdOfOrder(self):
+        return self.custid
 
     
-    # def getOrderType(self):
+    def getOrderType(self):
+        return self.sType
 
     
-    # def getDestinationAddress(self):
+    def getDestinationAddress(self):
+        return self.destination
 
     
     # toString() method
