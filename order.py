58edc212556937af2ded7a5791c61bd220bf1c1b
Daphne Domansi <ddomansi@stedwards.edu>
2020-04-03 01:58:07 +0000

    order.py edited online with Bitbucket

--- order.py
+++ order.py
@@ -4,10 +4,12 @@ class Order():
 
     # Class constructor. Set instance variables below. 
     # Each order instance will have a customer id, service type, and destination.
-    def __init__(self, custid, sType, destination):
+    def __init__(self, orderid, custid, sType, destination, timeOrderMade):
+        self.__orderid = orderid
         self.__custid = custid
         self.__sType = sType
         self.__destination = destination
+        self.__timeOrderMade = timeOrderMade
         
         
     #Defining our getter methods
@@ -15,6 +17,10 @@ class Order():
     # identifying the user who made the order from the front end (using session storage, might need to change up a little of 
     # back end for this 
     # but not sure yet).
+    def getOrderIdOfOrder(self):
+        resturn self.__orderid
+        
+        
     def getCustomerIdOfOrder(self):
         return self.__custid
 
@@ -25,11 +31,15 @@ class Order():
     
     def getDestinationAddress(self):
         return self.__destination
+        
+        
+    def getTimeOrderMade(self):
+        return self.__timeOrderMade
 
     
     # toString() method
     def __str__(self):
-        return self.getCustomerIdOfOrder() + " " + self.getOrderType() + " " + self.getDestinationAddress()
+        return "Order ID: " + self.getOrderIdOfOrder + " Customer ID: "+ + self.getCustomerIdOfOrder() + " Order Type: " + self.getOrderType() + " Destination: " + self.getDestinationAddress() + " Order Made: " + self.getTimeOrderMade
         
     def getOrderInfo(self):
         return self.__str__
