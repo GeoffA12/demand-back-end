d74437547b193684231619e553c2fdb982912204
ddomansi <ddomansi@stedwards.edu>
2020-04-10 04:47:27 -0500

    unit tests

--- UNITTEST/test-demand-customer-class.py
+++ UNITTEST/test-demand-customer-class.py
@@ -1,21 +1,43 @@
-# import unittest
-# import sys
-# sys.path.insert(1, '../')
-# from customers import Customer
+import unittest
+import sys
+sys.path.insert(1, '../')
+from customers import Customer
 
-# class TestDemandCustomerClass(unittest.TestCase):
+postBody = {
+    'username': username,
+    'email': email,
+    'password': password,
+    'firstname': firstname,
+    'lastname': lastname,
+    'phonenumber': phonenumber
+    }
 
-#     # Write your test cases here
+class TestDemandCustomerClass(unittest.TestCase):
 
-#     def testCustomerName(self):
+    # Write your test cases here
+    def testCreateCustomer(self):
+        cust = Customer(**postBody)
+        
+        self.assertEqual(username, cust.username)
+        self.assertEqual(email, cust.email)
+        self.assertEqual(password, cust.password)
+        self.assertEqual(firstname, cust.firstname)
+        self.assertEqual(lastname, cust.lastname)
+        self.assertEqual(phonenumber, cust.phonenumber)
+        self.assertEqual(custid, cust.custIDs)
+        
+        print(cust)
+        print()
+        
+    def testCustomerUsername(self):
+        cust = Customer(**postBody)
+        
+        self.assertEqual(username, cust.username)
+        self.assertEqual(custid, cust.custIDs)
+        # confusion
+        
+        print(cust)
 
-
-
-
-
-
-
-
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
 
--- UNITTEST/test-demand-order-class.py
+++ UNITTEST/test-demand-order-class.py
@@ -1,20 +1,23 @@
-# import unittest
-# import sys
-# sys.path.insert(1, '../')
-# from customers import Customer
+import unittest
+import sys
+from test.test_copy import order_comparisons
+sys.path.insert(1, '../')
+from customers import Customer
 
-# class TestDemandCustomerClass(unittest.TestCase):
+class TestDemandCustomerClass(unittest.TestCase):
 
-#     # Write your test cases here
-
-#     def testCustomerName(self):
-
-
-
-
-
-
-
-
-# if __name__ == '__main__':
-#     unittest.main()
+    # Write your test cases here
+    def testCreateOrder(self):
+        order = Order(123,321, ServiceType.DRYCLEANING,'destination?',2018-03-29T13:34:00.000)
+        
+        self.assertEqual(123, order.orderid)
+        self.assertEqual(321, order.custid)
+        self.assertEqual(ServiceType.DRYCLEANING, order.serviceType)
+        self.assertEqual('destination?', order.destination)
+        self.assertEqual(2018-03-29T13:34:00.000, order.timeOrderMade)
+        
+        print(order)
+        print()     
+        
+if __name__ == '__main__':
+    unittest.main()
