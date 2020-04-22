69201c90edf527c71fd2709c34472b7714795706
Jeffrey Ng <komoto415@gmail.com>
2020-04-22 17:22:02 -0500

    moved all database logic into utils module

--- team22-demand-webservice.py
+++ team22-demand-webservice.py
@@ -1,16 +1,13 @@
 import http.server
 from http.server import BaseHTTPRequestHandler
 import json
-import urllib.parse
-import mysql.connector as sqldb
+import utils.databaseutils as databaseutils
 import requests
+from enums.servicetype import ServiceType
 
-# move to utils ?!
-def connectToSQLDB():
-    return sqldb.connect(user='root', password='password', database='team22demand', port=6022)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    ver = '1.0'
+    ver = '0.1.2'
 
     # how to convert the body from a string to a dictionary
     # use 'loads' to convert from byte/string to a dictionary!
@@ -20,16 +17,19 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         return json.loads(body)
 
     def do_POST(self):
+        status = 404
         path = self.path
         print(path)
-        responseDict = {'Success': False} 
+        responseDict = {'Success': False}
 
         if '/demand/order/req' in path:
+            status = 400
             dictionary = self.getPOSTBody()
             # to access a specific key from the dictionary:
             print(dictionary)
             username = dictionary['username']
             serviceType = dictionary['serviceType']
+            serviceType = ServiceType.translate(serviceType)
             destination = dictionary['destination']
             timeOrderMade = dictionary['timeOrderMade'][:-5]
 
@@ -38,25 +38,32 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             print(destination)
             print(timeOrderMade)
 
-            sqlConnection = connectToSQLDB()
-            cursor = sqlConnection.cursor()
-            cursor.execute('SELECT custid FROM customers WHERE username = %s OR email = %s', (username,username))
-            custid = cursor.fetchone()
+            # sqlConnection = databaseutils.connectToSQLDB()
+            # cursor = sqlConnection.cursor()
+            # cursor.execute('SELECT custid FROM customers WHERE username = %s OR email = %s', (username,username))
+            # custid = cursor.fetchone()
+            custid = databaseutils.getCustomerIDByCredentials(username)
             print(custid)
-            if custid:
+            if custid is not None:
                 custid = custid[0]
                 humanReadable = destination.pop('humanReadable')
                 print(custid)
-                cursor.execute('INSERT INTO orders VALUES (Null, %s, %s, %s, %s)',
-                               (custid, serviceType.lower(), humanReadable, timeOrderMade))
-                sqlConnection.commit()
-                cursor.execute('SELECT orderid FROM orders WHERE custid = %s AND date_ordered = %s', (custid,timeOrderMade))
-                orderid = cursor.fetchone()[0]
+                # cursor.execute('INSERT INTO orders VALUES (Null, %s, %s, %s, %s)',
+                #                (custid, serviceType.lower(), humanReadable, timeOrderMade))
+                # sqlConnection.commit()
+                # cursor.execute('SELECT orderid FROM orders WHERE custid = %s AND date_ordered = %s', (custid,timeOrderMade))
+                # orderid = cursor.fetchone()[0]
+                orderData = (custid, serviceType.lower(), humanReadable, timeOrderMade,)
+                databaseutils.storeOrder(orderData)
+
+                data = (custid, timeOrderMade,)
+                orderid = databaseutils.getOrderID(data)
+
                 print(orderid)
                 custid = int(custid)
                 orderid = int(orderid)
-                
-                lat,long = destination['lat'], destination['lon']
+
+                lat, lon = destination['lat'], destination['lon']
 
                 # rebuild post body dictionary
                 orderDict = {
@@ -64,30 +71,21 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                     'custid': custid,
                     'orderid': orderid,
                     'destination': {
-                        'lat': lat, #must always be between -90 and 90
-                        'lon': long
-                        },
+                        'lat': lat,  # must always be between -90 and 90
+                        'lon': lon
+                    },
                     'timeOrderMade': timeOrderMade
-                    }
-                # serviceType, timeOrderMade, destination (lat,long(floats)), custid(int), orderid(int)
-                
-                '''
-                appened customer id into dictionary as well. 
-                attribute will be 'customerID'
-                '''
+                }
+
                 # Make API call to vehicleRequest, POSTing our order dictionary. Our API will need partial order
                 # dictionary information.
                 print(orderDict)
                 orderDict = json.dumps(orderDict)
-                response = requests.post('https://supply.team22.softwareengineeringii.com/supply/vehicles/req', orderDict)
+                response = requests.post('https://supply.team22.softwareengineeringii.com/supply/vehicles/req',
+                        orderDict)
                 status = response.status_code
-                # status = 200
                 if status == 200:
                     responseDict['Vehicle Info'] = response.json()
-            else:
-                status = 400
-        else:
-            status = 404
 
         self.send_response(status)
         self.end_headers()
@@ -95,35 +93,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         bytesStr = res.encode('utf-8')
         self.wfile.write(bytesStr)
 
-    def do_GET(self):
-        path = self.path
-        print(path)
-        responseDict = {'Success': False}
-        if '/orderConfirmation' in path:
-            orderid = 123
-            PARAMS = {'oid':orderid}
-            response = requests.get('https://supply.team22.softwareengineeringii.com/etaRequest', PARAMS)
-            data = response.json()
-            status = response.status_code
-            if status == 200:
-                responseDict = data
-        else:
-            status == 404
-            
-            #sqlConnection = connectToSQLDB()
-            #cursor = sqlConnection.cursor()
-            #cursor.execute('SELECT orderid FROM orders WHERE ')
-        print('got')
-        self.send_response(status)
-        self.end_headers()
-        res = json.dumps(responseDict)
-        byteStr = res.encode('utf-8')
-        self.wfile.write(byteStr)
-
-    def do_OPTIONS(self):
-        print('options')
-        self.send_response(200)
-        self.end_headers()
 
 def main():
     # Define the port your server will run on:
@@ -138,6 +107,7 @@ def main():
     # instructions on how to run without blocking other commands frombeing
     # executed in your terminal!
     httpServer.serve_forever()
+
 
 if __name__ == "__main__":
     main()
