b11329a5a95b2f14ad99a12a4272e8b21e21d18b
jng415 <jng@stedwards.edu>
2020-04-22 22:30:11 +0000

    Switched to the correct database that we connect to

--- utils/databaseutils.py
+++ utils/databaseutils.py
@@ -8,7 +8,7 @@ def connectToSQLDB():
     import os
     import mysql.connector as sqldb
     password = os.getenv('DB_PASSWORD')
-    return sqldb.connect(user='root', password=password, database='team22supply', port=6022, buffered=True)
+    return sqldb.connect(user='root', password=password, database='team22demand', port=6022, buffered=True)
 
 
 def getCustomerIDByCredentials(username):
@@ -16,7 +16,9 @@ def getCustomerIDByCredentials(username):
     cursor = sqlConnection.cursor(buffered=True)
 
     statement = 'SELECT custid FROM customers WHERE username = %s OR email = %s'
-    cursor.execute(statement, (username, username))
+    print(statement)
+    print((username, username,))
+    cursor.execute(statement, (username, username,))
     custid = cursor.fetchone()
 
     cursor.close()
